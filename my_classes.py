fec408bde06470239312e9b62263915e6b2e862d
J-u-lia <j.roessler@mci4me.at>
2025-04-01 15:34:28 +0200

    Aufgabe62

--- my_classes.py
+++ my_classes.py
@@ -1,31 +1,42 @@
-# Klasse Subject definieren
-# estimate_max_hr als Methode definieren
-class Subject():
-    def __init__(self, first_name, last_name, sex, age):
-        self.first_name = first_name
-        self.last_name = last_name
+import datetime
+from my_functions import estimate_max_hr
+
+# Elternklasse Person definieren
+class Person():
+    def __init__(self, name, sex, date_of_birth):
+        self.name = name
         self.sex = sex
-        self.age = age
-        pass
-    def estimate_max_hr(self):
-        """
-        A function that estimates the maximum heart rate of a subject"""
-        pass
+        self.__date_of_birth = datetime.datetime.strptime(date_of_birth, "%Y-%m-%d").date()  # Private attribute
+    def get_age_years(self):
+        today = datetime.date.today()
+        age_years = datetime.datetime.now().year - self.__date_of_birth.year
+        # um zu prüfen, ob der Geburtstag in diesem Jahr bereits war 
+        if (today.month, today.day) < (self.__date_of_birth.month, self.__date_of_birth.day):
+            age_years -= 1
+        return age_years
 
+# Kindklasse Subject und Supervisor definieren
+class Subject(Person):
+    def __init__(self, name, sex, date_of_birth):
+        super().__init__(name, sex, date_of_birth)  # Erben von Elternklasse
+    
+    # Alter ausrechnen mit dem __date_of_birth private Attribut
+    def get_subject_age(self):
+        return self.get_age_years()
+    
+    # estimate_max_hr als Methode definieren
+    def estimate_subject_max_hr(self):
+        """ Berechnet die maximale Herzfrequenz für das Subject """
+        age_years = self.get_subject_age()
+        return estimate_max_hr(age_years, self.sex)
 
-
-
-
-# Klasse Supervisor definieren
-class Supervisor():
-    def __init__(self, first_name, last_name, sex, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.sex = sex
-        self.age = age
-        pass
-
-
+class Supervisor(Person):
+    def __init__(self, name, sex, date_of_birth):
+        super().__init__(name, sex, date_of_birth)
+    
+    # Alter ausrechnen mit dem __date_of_birth Attribut
+    def get_supervisor_age(self):
+        return self.get_age_years()
 
 
 # Klasse Experiment definieren
@@ -37,7 +48,7 @@ class Experiment():
         self.supervisor = supervisor
         self.experiment_type = experiment_type
         self.date = date
-        pass
+        
     def add_subject(self, subject):
         """
         A function that adds a subject to an experiment"""
@@ -48,3 +59,9 @@ class Experiment():
         A function that adds a supervisor to an experiment"""
         self.supervisor = supervisor
         pass
+
+
+# Testen der Klassen
+p = Subject("Julia", "female", "2006-06-09")
+print(f"Alter: {p.get_age_years()} Jahre")
+print(f"Maximale Herzfrequenz: {p.estimate_subject_max_hr()} bpm")
