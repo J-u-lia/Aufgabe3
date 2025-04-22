3fc3f41fb919fd6afc6482fab1eb19a6820ee4d4
J-u-lia <j.roessler@mci4me.at>
2025-04-22 13:14:43 +0200

    Aufgabe8

--- my_classes.py
+++ my_classes.py
@@ -1,12 +1,15 @@
 import datetime
 from my_functions import estimate_max_hr
+import requests   # davor noch pip install requests im Terminal ausführen, Importiere die requests-Bibliothek für HTTP-Anfragen
 
 # Elternklasse Person definieren
 class Person():
-    def __init__(self, name, sex, date_of_birth):
+    id_counter = 0  # ID-Attribut für die Person, wird automatisch hochgezählt
+    def __init__(self, name, sex, date_of_birth, id=None):
         self.name = name
         self.sex = sex
         self.__date_of_birth = datetime.datetime.strptime(date_of_birth, "%Y-%m-%d").date()  # Private attribute
+        self.id = id  # ID ist optional, wird aber in der Datenbank benötigt, um die Person zu identifizieren ---> momentan wird nur ein name an den server gesendet, die API-Vorlage versucht aber eine id zu finden
     def get_age_years(self):
         today = datetime.date.today()
         age_years = datetime.datetime.now().year - self.__date_of_birth.year
@@ -14,11 +17,31 @@ class Person():
         if (today.month, today.day) < (self.__date_of_birth.month, self.__date_of_birth.day):
             age_years -= 1
         return age_years
+    
+    # Person verfügen über eine Methode, die eine neue Person an den Webserver http://127.0.0.1:5000 sendet
+    def put(self):
+        if self.id is None:  # Wenn die ID nicht gesetzt ist, dann wird sie automatisch hochgezählt
+            Person.id_counter += 1
+            self.id = Person.id_counter  # ID der Person wird gesetzt
+        
+        url = "http://127.0.0.1:5000/person/"    # wichtig dass der URL mit /person/ endet, sonst wird die Person nicht angelegt und es kommt ein error heraus, obwohl bei seinem geclonten repository eine meldung kommt das was verscuht worden ist zu putten
+        data = {
+            "id": self.id,  # ID der Person, die aktualisiert werden soll
+            "name": self.name,
+            }
+        response = requests.post(url, json=data)
+        if response.status_code == 201:  # der response.status_code 201 bedeutet, dass eine neue Person erfolgreich created wurde, 200 würde nur bedeutn, dass die Anfrage erfolgreich war
+            returned_data = response.json()
+            self.id = returned_data.get("id")  # ID von der Antwort speichern
+            print("Data sent successfully, ID:, {self.id}")
+        else:
+            print("Error sending data", response.status_code)
 
 # Kindklasse Subject und Supervisor definieren
 class Subject(Person):
-    def __init__(self, name, sex, date_of_birth):
-        super().__init__(name, sex, date_of_birth)  # Erben von Elternklasse
+    def __init__(self, name, sex, date_of_birth, id=None, email=None):
+        super().__init__(name, sex, date_of_birth, id)  # Erben von Elternklasse
+        # self.email = email
     
     # Alter ausrechnen mit dem __date_of_birth private Attribut
     def get_subject_age(self):
@@ -29,6 +52,20 @@ class Subject(Person):
         """ Berechnet die maximale Herzfrequenz für das Subject """
         age_years = self.get_subject_age()
         return estimate_max_hr(age_years, self.sex)
+    
+    # Subject hat jetzt eine Methode, die eine PUT-Anfrage an den Webserver sendet, um die E-Mail-Adresse zu aktualisieren mit dem gleichen vornamen wie aus put()
+    def update_email(self, email):
+        url = "http://127.0.0.1:5000/person/"  # wichtig, dass /person/email dasteht, sonst passiert das gleiche wie vorher bei put()
+        data = {
+            "id": self.id,  # ID der Person, die aktualisiert werden soll
+            "name": self.name,
+            "email": email}
+
+        response = requests.put(url, json=data)
+        if response.status_code == 200:
+            print("Data updated successfully", response.json())
+        else:
+            print("Error updating data", response.status_code)
 
 class Supervisor(Person):
     def __init__(self, name, sex, date_of_birth):
--- test.py
+++ test.py
@@ -3,17 +3,20 @@ from my_classes import Subject, Supervisor, Experiment
 
 if __name__ == "__main__":
     # Create a supervisor and a subject
-    supervisor = mf.build_person("John", "Doe", "male", 30)
+    # supervisor = mf.build_person("John", "Doe", "male", 30)
     # leistungstest kreieren
-    supervisor = Supervisor("Max", "Mustermann", "male", "35")
-    subject = Subject("Maria", "Musterfrau", "female", "30")
-    subject.estimate_max_hr()
+    # supervisor = Supervisor("Max Mustermann", "male", "1980-01-01")
+    subject = Subject("Maria Musterfrau", "female", "2000-01-01", id="123456")
+    subject.put()    # Person auf Server anlegen
+    subject.update_email("MariaMusterfrau@example.com")    # E-Mail-Adresse aktualisieren
+    # subject.estimate_max_hr()
+#
+    # experiment = Experiment("Leistungstest","Maria_Musterfrau", "Max_Mustermann", "max_estimate_hr", "2025-04-16")
+    # experiment.add_subject(subject)
+    # experiment.add_supervisor(supervisor)
 
-    experiment = Experiment("Leistungstest","Baum", "Max_Mustermann", "max_estimate_hr", "2025-03-29")
-    experiment.add_subject(subject)
-    experiment.add_supervisor(supervisor)
 
 
-
-print(supervisor)
-print(experiment)
+# print(supervisor)
+print(subject)
+# print(experiment)
